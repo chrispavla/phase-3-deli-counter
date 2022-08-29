b7e20dcdf79dd4efbf5e90a91a96288b25c0a095
Kristina <kristina.hodges7@gmail.com>
2022-08-29 13:04:03 -0400

    commit tree a5f2dc3d45724e1f9fd62f349066dcbdc26108f3

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+
+
+def line arr
+  message = []
+
+  if arr.length == 0
+    puts "The line is currently empty."
+  else
+  new_list = arr.each_with_index do |person, index|
+    message << "The line is currently: #{index + 1}. #{person}"
+    end
+    puts message
+  end
+end
+
+
